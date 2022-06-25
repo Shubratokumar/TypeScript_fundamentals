e91e2cc27a30782d78afb819bc39d28903eda099
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-06-26 01:00:01 +0600

    enum -typescript

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,21 @@
 // user[0].toString();
 // user[1].concat();
 
+// Enums : list of related constant
 
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+// PascleCase convension
+// enum Size { Small = 0 , Medium = 1, Large = 2}  // by default
+// enum Size { Small = 1 , Medium , Large } // we have to set the first element value later increase so on. If we set string value then we have to set explicitly. Rest of the number will set the compiler while compling the code.
+
+// declare enum with const generate more optimize code
+const enum Size { Small = 1 , Medium , Large }
+
+//  use cases 
+let mySize: Size = Size.Medium;
+console.log(mySize);
 
 
